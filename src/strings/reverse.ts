102cb819222fdb68d04dfad414764a277ca02feb
DIMITRY <dmitry.tezhelnikov@gmail.com>
2023-04-29 19:44:06 +0300

    reverse text optimization

--- src/strings/reverse.ts
+++ src/strings/reverse.ts
@@ -1,4 +1,10 @@
 // Перевернуть текст
 export const reverse = (text: string): string => {
-    return text.split('').reverse().join('');
+    const chars = text.split('');
+    for (let i = 0; i <= Math.floor(chars.length / 2); i++) {
+        const temp = chars[i];
+        chars[i] = chars[chars.length - i - 1];
+        chars[chars.length - i - 1] = temp;
+    }
+    return chars.join('');
 };
