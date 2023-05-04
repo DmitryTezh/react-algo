fbfc5093e1490341c61ea78b1719fbdb591713e2
DIMITRY <dmitry.tezhelnikov@gmail.com>
2023-05-04 17:08:38 +0300

    permutation without recursion

--- src/strings/permutations.ts
+++ src/strings/permutations.ts
@@ -1,21 +1,45 @@
-const evaluatePermutations = (elements: string[]): string[][] => {
-    if (elements.length < 2) {
+const swapArray = (elements: string[], i: number, j: number): string[] => {
+    const copy = [...elements];
+    if (i !== j) {
+        const temp = copy[i];
+        copy[i] = copy[j];
+        copy[j] = temp;
+    }
+    return copy;
+};
+
+const permute = (elements: string[], onlyUnique?: boolean): string[][] => {
+    const seen = onlyUnique ? new Set<string>([elements.join('')]) : undefined;
+    const allPermutations: string[][] = [elements];
+    for (let i = 0; i < elements.length - 1; i++) {
+        const levelPermutations: string[][] = [];
+        for (const permutation of allPermutations) {
+            for (let j = i + 1; j < elements.length; j++) {
+                const swapPermutation = swapArray(permutation, i, j);
+                const key = swapPermutation.join('');
+                if (seen == null || !seen.has(key)) {
+                    seen?.add(key);
+                    levelPermutations.push(swapPermutation);
+                }
+            }
+        }
+        allPermutations.push(...levelPermutations);
+    }
+    return allPermutations;
+};
+
+const permuteRecursion = (elements: string[]): string[][] => {
+    if (elements.length === 1) {
         return [elements];
     }
     const element = elements[elements.length - 1];
-    const permutations = evaluatePermutations(elements.slice(0, -1));
-    return permutations.flatMap(p => [
-        ...p.map((el, index) => {
-            const left = p.slice(0, index);
-            const right = p.slice(index);
-            const permutation = [...left, element, ...right];
-            return permutation;
-        }),
+    return permuteRecursion(elements.slice(0, -1)).flatMap(p => [
+        ...p.map((_, index) => [...p.slice(0, index), element, ...p.slice(index)]),
         [...p, element],
     ]);
 };
 
 // Все перестановки множества
-export const permutations = (elements: string): string[][] => {
-    return evaluatePermutations(elements.split(''));
+export const permutations = (elements: string, onlyUnique?: boolean): string[][] => {
+    return permute(elements.split(''), onlyUnique);
 };
